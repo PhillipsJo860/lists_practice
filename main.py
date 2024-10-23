945f0b21de7738647a2e9698e1b1eea25dcb5793
SoggyCereal <PhillipsJo860@northwested.org>
2024-10-23 10:01:41 -0400

    added content

---
+++ main.py
@@ -0,0 +1,24 @@
+# Joshua Phillips
+# 10/21/24
+# Lists. OwO
+import random
+
+friends = ['No-one(Im alone)', 'Lucas', 'Hunter(Keasey)', 'Sherbert Eliot Phillips(my cat)']
+
+# 3-1:
+# print(f'My first fri1
+# end is \'Drumroll\'... {friends[0]}... Second we have a forced volenteer: {friends[1]}! Next up, the one whos stuck in my basement(I dont know how he got there): {friends[2]}! And lastly we have my precious, {friends[3]}! Thats all folks!')
+
+# 3-2
+# print('Hello, and welcome to Joshua\'s website! Im going to try and guess which of his friends you are(If your not then leave.)')
+# friend_foe = int(input('Are you my friend?(1 for yes 2 for no): '))
+
+# if friend_foe == 1:
+#     friend = random.randint(1, len(friends))
+#     print('I bet you are ' + friends[friend-1] + ' arent you? I knew it! Hello there and how is your day going?')
+# elif friend_foe == 2:
+#     print('Scram fiend!')
+# else:
+#     print('Run that by me again? I dont quite understand your answer.(I only understand 1 or 2.)')
+
+# 3-3
